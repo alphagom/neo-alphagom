7a6f49261e003d99316b9d9606c4c70bc69a2041
Hyunjung Lee <hyunjung20222@gmail.com>
2022-12-12 16:21:23 +0900

    fix: 파일명 Pascal case 변경 (#8)

--- front/src/pages/main/mainPage.tsx
+++ front/src/pages/main/mainPage.tsx
@@ -2,19 +2,19 @@ import React, { useState } from 'react'
 import { getTest } from 'api/mainApi'
 
 function MainPage() {
-    // =============== TEST CODE ===============
-    const [test, setTest] = useState('')
-    getTest('').then((result) => {
-        setTest(result.data.msg)
-    })
-    // =========================================
+  // =============== TEST CODE ===============
+  const [test, setTest] = useState('')
+  getTest('').then((result) => {
+    setTest(result.data.msg)
+  })
+  // =========================================
 
-    return (
-        <div>
-            <p>MainPage</p>
-            <p>{test}</p>
-        </div>
-    )
+  return (
+    <div>
+      <p>MainPage</p>
+      <p>{test}</p>
+    </div>
+  )
 }
 
 export default MainPage
