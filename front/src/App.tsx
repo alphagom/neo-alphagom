718203a674485f04634e0685781e6bcb282bb179
Hyunjung Lee <hyunjung20222@gmail.com>
2022-12-12 16:23:12 +0900

    chore: recoil 적용 (#8)

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 import './App.css'
 
 function App() {
-    return (
-        <div>
-            <h1>Hello World!</h1>
-        </div>
-    )
+  return (
+    <div>
+      <h1>Hello World!</h1>
+    </div>
+  )
 }
 
 export default App
--- front/src/index.tsx
+++ front/src/index.tsx
@@ -1,26 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { RecoilRoot } from 'recoil'
 import { RouterProvider } from 'react-router-dom'
 import './index.css'
 
 import App from 'App'
-import reportWebVitals from './reportWebVitals'
+// import reportWebVitals from './reportWebVitals'
 import router from './router/router'
 
 import { Worker } from './mock/Worker'
 
 if (process.env.NODE_ENV === 'development') {
-    Worker.start()
+  Worker.start()
 }
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-    <React.StrictMode>
-        <App />
-        <RouterProvider router={router} />
-    </React.StrictMode>
+  <React.StrictMode>
+    <RecoilRoot>
+      <App />
+      <RouterProvider router={router} />
+    </RecoilRoot>
+  </React.StrictMode>
 )
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals(console.log)
+// reportWebVitals(console.log)
