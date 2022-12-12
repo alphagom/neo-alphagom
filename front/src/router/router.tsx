f170473814846fd2fd49a67a630568cc7fb83931
Hyunjung Lee <hyunjung20222@gmail.com>
2022-12-12 16:24:48 +0900

    feature: recoil 과 TS 예제 작성2 (#8)

--- front/src/router/router.tsx
+++ front/src/router/router.tsx
@@ -1,23 +1,38 @@
 import React from 'react'
-import { createBrowserRouter } from 'react-router-dom'
+import { createBrowserRouter, Outlet } from 'react-router-dom'
 
 import MainPage from 'pages/main/MainPage'
 import PortfolioPage from 'pages/portfolio/PortfolioPage'
 import MyportfolioPage from 'pages/myportfolio/MyportfolioPage'
+import NavBar from 'components/NavBar'
 
+/**
+ * Router, Routes 로 감싸는 대신 RouteProvider 사용 시
+ * element 에 명시해줘야 링크 가능
+ */
 const router = createBrowserRouter([
-    {
+  {
+    element: (
+      <>
+        <NavBar />
+        <Outlet />
+      </>
+    ),
+    children: [
+      {
         path: '/',
         element: <MainPage />,
-    },
-    {
+      },
+      {
         path: '/portfolio',
         element: <PortfolioPage />,
-    },
-    {
+      },
+      {
         path: '/myportfolio',
         element: <MyportfolioPage />,
-    },
+      },
+    ],
+  },
 ])
 
 export default router
