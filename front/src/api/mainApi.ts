1e91df75a4ed991a64a638d3c74f073c93fd3942
Hyunjung Lee <hyunjung20222@gmail.com>
2022-12-14 09:44:13 +0900

    feature : MSW test 기존 코드 제거 (#7)

--- front/src/api/mainApi.ts
+++ front/src/api/mainApi.ts
@@ -1,5 +1,1 @@
-import api from './api'
-import { testApi } from './url'
-
-// eslint-disable-next-line import/prefer-default-export
-export const getTest = (accessToken: string) => api(accessToken).get(testApi)
+export default {}
