7e9c7c413cbbdf3972d530fa206f79c8009c7ff2
Hyunjung Lee <hyunjung20222@gmail.com>
2022-12-14 13:58:46 +0900

    style : prettier 2칸 적용 (#5)

--- front/src/pages/myportfolio/MyportfolioPage.tsx
+++ front/src/pages/myportfolio/MyportfolioPage.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
 function MyportfolioPage() {
-    return (
-        <div>
-            <p>MyportfolioPage</p>
-        </div>
-    )
+  return (
+    <div>
+      <p>MyportfolioPage</p>
+    </div>
+  )
 }
 
 export default MyportfolioPage
