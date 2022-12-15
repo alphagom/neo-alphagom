aa38fa861aa7ad10da88967ff4083e095a668f1d
Hyunjung Lee <hyunjung20222@gmail.com>
2022-12-15 14:44:28 +0900

    feature : 포트폴리오 항목 순서 atom 생성 (#39)

--- front/src/store/portfolioState.ts
+++ front/src/store/portfolioState.ts
@@ -1,1 +1,9 @@
-export default {}
+import { atom, selector } from 'recoil'
+
+import { PortfolioItems, PorfolioItemsType } from './portfolioType'
+
+// 포트폴리오 항목 순서 state
+export const pfItemsOrderState = atom<Array<PorfolioItemsType>>({
+  key: 'pfItemsOrderState',
+  default: PortfolioItems,
+})
