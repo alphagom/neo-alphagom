6620a53dad8d34dd75d4999cef6df416368372dc
Hyunjung Lee <hyunjung20222@gmail.com>
2022-12-16 15:05:11 +0900

    feature : 포트폴리오 항목바/디자인/입력폼 순서 연결 (#63)

--- front/src/pages/portfolio/PortfolioPage.tsx
+++ front/src/pages/portfolio/PortfolioPage.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import PorfolioItemBar from './components/PortfolioItemBar'
 import './PortfolioPage.css'
+import PortfolioWithDesign from './components/PortfolioWithDesign'
 
 function PortfolioPage() {
   return (
-    <div className="portfolio-page">
+    <div className="portfolio-page" style={{ display: 'flex' }}>
       <PorfolioItemBar />
+      <PortfolioWithDesign />
     </div>
   )
 }
