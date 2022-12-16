b27b9643be780fc9a27200ba582252c041dca049
Hyunjung Lee <hyunjung20222@gmail.com>
2022-12-16 15:00:24 +0900

    fix : 포트폴리오 항목별 입력폼 기본 코드 추가 (#63)

--- front/src/pages/portfolio/components/itemforms/InfoForm.tsx
+++ front/src/pages/portfolio/components/itemforms/InfoForm.tsx
@@ -1,1 +1,7 @@
-export default {}
+import React from 'react'
+
+function InfoForm() {
+  return <div>기본정보 입력 폼</div>
+}
+
+export default InfoForm
--- front/src/pages/portfolio/components/itemforms/ProjectForm.tsx
+++ front/src/pages/portfolio/components/itemforms/ProjectForm.tsx
@@ -1,1 +1,7 @@
-export default {}
+import React from 'react'
+
+function ProjectForm() {
+  return <div>프로젝트 입력 폼</div>
+}
+
+export default ProjectForm
