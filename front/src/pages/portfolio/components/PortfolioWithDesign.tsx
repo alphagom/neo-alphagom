94e02ef5f2c1fdd08f2aec27ea26874585be1d68
Hyunjung Lee <hyunjung20222@gmail.com>
2022-12-17 20:57:23 +0900

    refactor : 인터페이스 타입명 수정 (#65)

--- front/src/pages/portfolio/components/PortfolioWithDesign.tsx
+++ front/src/pages/portfolio/components/PortfolioWithDesign.tsx
@@ -6,7 +6,7 @@ import { useRecoilState } from 'recoil'
 // atom 과 selector import
 import { pfItemsOrderState } from 'store/portfolioState'
 // 사용할 type import
-import { PorfolioItemsType } from 'store/portfolioType'
+import { IporfolioItemsType } from 'store/portfolioType'
 
 function PortfolioWithDesign() {
   // 인덱스 순서가 담긴 atom과 selector을 선언
@@ -17,7 +17,7 @@ function PortfolioWithDesign() {
       <span>
         {/* idx 는 고유한 key 값으로 쓰면 안됨 (예외적인 상황 제외) 
         여기서는 순서가 변경될 때마다 바뀌어야하므로 idx 로 key 값 지정 */}
-        {pfItemsOrder.map((item: PorfolioItemsType, idx) => (
+        {pfItemsOrder.map((item: IporfolioItemsType, idx) => (
           <div key={idx}>
             <item.itemForm />
             <item.itemDesign />
