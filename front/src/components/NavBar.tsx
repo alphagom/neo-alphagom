bdacc81fb7c558199bff0eecb301f8d2599cabdb
foursoob <foursoob@naver.com>
2022-12-19 03:05:21 +0900

    feature: Navbar로 미디어쿼리 테스트 (#53)

--- front/src/components/NavBar.tsx
+++ front/src/components/NavBar.tsx
@@ -1,17 +1,19 @@
 import React, { useState, ComponentProps } from 'react'
 import { Link } from 'react-router-dom'
-import './NavBar.css'
+import './NavBar.scss'
 
 function NavBar() {
   return (
     <div className="temp-navbar">
-    <Link to="/" className="nav-items">
+      <Link to="/" className="nav-items">
         <div>홈</div>
       </Link>
-    <Link to="/portfolio" className="nav-items">
+      <div className="nav-item-line" />
+      <Link to="/portfolio" className="nav-items">
         <div>포트폴리오 만들기</div>
       </Link>
-    <Link to="/myportfolio" className="nav-items">
+      <div className="nav-item-line" />
+      <Link to="/myportfolio" className="nav-items">
         <div>내 포트폴리오</div>
       </Link>
     </div>
