3825440e0f9641e6bf0c495bc9ab7b4baf5e6ee9
김수빈 <foursoob@naver.com>
2022-12-19 18:25:21 +0900

    feature: 포트폴리오 항목 디자인_기본정보 디자인 세팅 (#38)

--- front/src/pages/portfolio/components/itemdesigns/base/InfoDesigns.tsx
+++ front/src/pages/portfolio/components/itemdesigns/base/InfoDesigns.tsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import './Base.scss'
 
 function InfoDesign() {
-  return <div>기본정보 디자인 폼</div>
+  return (
+    <div>
+      <div className='base-title'>기본정보 디자인 폼</div>
+      <div className='base-card'>
+        <div className='base-item-profile'>
+          <img src="https://img.insight.co.kr/static/2020/09/02/700/98j4263429d06416wy82.jpg" alt="" />
+        </div>
+        <div className='base-item'>
+          <div className='base-item-title'>이름</div>
+          <div className='base-item-content'>내용</div>
+        </div>
+      </div>
+    </div>)
 }
 
 export default InfoDesign
---
+++ front/src/pages/portfolio/components/itemdesigns/itemdesignsbaseitem/BaseItemTitle.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function BaseItemTitle() {
+  return <div className='base-item-title'>BaseItemTitle</div>
+}
+
+export default BaseItemTitle
