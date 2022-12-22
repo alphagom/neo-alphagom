7d1395bd4891a1281aaa9fd0bee69a0ee24387df
foursoob <foursoob@naver.com>
2022-12-22 22:27:24 +0900

    feature: 포폴 디자인/입력폼 전역 state 초안 (#38)

--- front/src/store/portfolioState.ts
+++ front/src/store/portfolioState.ts
@@ -1,9 +1,21 @@
 import { atom, selector } from 'recoil'
 
-import { PortfolioItems, PorfolioItemsType } from './portfolioType'
+import { PortfolioItems, PorfolioItemsType, IPFBaseType } from './portfolioType'
 
 // 포트폴리오 항목 순서 state
 export const pfItemsOrderState = atom<Array<PorfolioItemsType>>({
   key: 'pfItemsOrderState',
   default: PortfolioItems,
+})
+
+// 포트폴리오 디자인 베이스 아이템 Title state
+export const pfDesignBaseTitleState = atom<IPFBaseType>({
+  key: 'pfDesignBaseTitleState',
+  default: {
+    itemsType: 1,
+    isTitle: true,
+    title: '제목',
+    content: '내용',
+    isEditable: false,
+  },
 })
--- front/src/store/portfolioType.ts
+++ front/src/store/portfolioType.ts
@@ -112,3 +112,83 @@ export const PortfolioItems: PorfolioItemsType[] = [
     itemForm: CoverletterForm,
   },
 ]
+
+// 포트폴리오 디자인 베이스 아이템 타입
+// 베이스 친구는 입력폼에도 쓰입니다!
+export interface IPFBaseType {
+  itemsType: number // 어떤 항목의 어떤 타입인지
+  isTitle: boolean // 제목이니? 내용이니?
+  title: string // 제목이니?
+  content: string // 내용이니? 제목이라면 빈문자열
+  isEditable: boolean // 내용이라면 수정 가능하니? 아니니?
+}
+
+// 포트폴리오 항목 itemsType Enum
+export enum EPFItemsEnum {}
+// 이거 어캄,,
+
+// 이놈들을 그냥 enum으로 만들어야하나? 아니면 하나에 한 enum씩 만들어 줘야할까?
+// 포트폴리오 기본정보 문자열
+export const PFInfoStr = [
+  '',
+  '이름',
+  '생년월일',
+  '이메일',
+  '깃허브 or 기술 블로그',
+]
+
+// 포트폴리오 학력 문자열
+export const PFEducationStr = [
+  '',
+  '학교',
+  '학교명',
+  '졸업 여부',
+  '편입 여부',
+  '입학일',
+  '졸업일',
+  '주전공',
+  '부전공',
+  '복수전공',
+  '평점/만점',
+  '연구 분야',
+  '연구 설명',
+]
+// 포트폴리오 디자인 프로젝트 문자열
+export const PFDesignProjectStr = [
+  '',
+  '프로젝트명',
+  '시작',
+  '종료',
+  '설명',
+  '팀원 수',
+  '담당 업무',
+  '관련 링크',
+  '성과',
+  '회고',
+  '이미지',
+]
+// 포트폴리오 대내외활동 문자열
+export const PFDesignActivitysStr = [
+  '',
+  '대내외활동',
+  '시작',
+  '종료',
+  '요약',
+  '상세',
+]
+// 포트폴리오 수상내역 문자열
+export const PFAwordsStr = ['', '상 이름', '상 설명', '날짜', '주관저']
+// 포트폴리오 경력 문자열
+export const PFCareerStr = [
+  '',
+  '직장명',
+  '재직상태',
+  '시작',
+  '종료',
+  '직급',
+  '직무',
+  '계약 형태',
+  '주요 성과',
+]
+// 포트폴리오 자기소개서 문자열
+export const PFCoverletterStr = ['', '제목', '내용']
