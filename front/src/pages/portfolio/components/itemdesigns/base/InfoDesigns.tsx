dadad80b0c06094b6f54735be62ac7abb3db76e6
foursoob <foursoob@naver.com>
2022-12-22 18:57:40 +0900

    feature: 포폴 항목 디자인_기본정보 컴포넌트 세팅(#38)

--- front/src/pages/portfolio/components/itemdesigns/base/InfoDesigns.tsx
+++ front/src/pages/portfolio/components/itemdesigns/base/InfoDesigns.tsx
@@ -1,7 +1,26 @@
 import React from 'react'
+import './Base.scss'
+import BaseItemTitle from '../itemdesignsbaseitem/BaseItemTitle'
+import BaseItemContent from '../itemdesignsbaseitem/BaseItemContent'
 
 function InfoDesign() {
-  return <div>기본정보 디자인 폼</div>
+  return (
+    <div>
+      <div className="base-title">기본정보 디자인 폼</div>
+      <div className="base-card">
+        <div className="base-item-profile">
+          <img
+            src="https://img.insight.co.kr/static/2020/09/02/700/98j4263429d06416wy82.jpg"
+            alt=""
+          />
+        </div>
+        <div className="base-item">
+          <BaseItemTitle />
+          <BaseItemContent />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default InfoDesign
---
+++ front/src/pages/portfolio/components/itemdesigns/itemdesignsbaseitem/BaseItemContent.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function BaseItemContent() {
+  return <div className="base-item-content">BaseItemContent</div>
+}
+
+export default BaseItemContent
