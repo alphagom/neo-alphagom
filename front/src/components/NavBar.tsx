3ff06f232c6daa8dfee003bcdeeffb931ef3fc01
mhlee <mihyun0921@gmail.com>
2022-12-22 20:15:20 +0900

    feature : NavBar 구현 (#42)

--- front/src/components/NavBar.tsx
+++ front/src/components/NavBar.tsx
@@ -1,21 +1,37 @@
-import React, { useState, ComponentProps } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
 import './NavBar.scss'
+import logo from '../assets/devgom.png'
+import title from '../assets/title.png'
+import UserIcon from './UserIcon'
 
 function NavBar() {
   return (
-    <div className="temp-navbar">
-      <Link to="/" className="nav-items">
-        <div>홈</div>
-      </Link>
-      <div className="nav-item-line" />
-      <Link to="/portfolio" className="nav-items">
-        <div>포트폴리오 만들기</div>
-      </Link>
-      <div className="nav-item-line" />
-      <Link to="/myportfolio" className="nav-items">
-        <div>내 포트폴리오</div>
-      </Link>
+    <div className="navbar">
+      <div className="navbar-content">
+        <a href="/">
+          <img src={logo} alt="devgom logo" width="155" />
+        </a>
+        <div className="navbar-menu">
+          <a href="/">
+            <img src={title} alt="devgom title" width="355" />
+          </a>
+          <div className="navbar-list">
+            <Link to="/" className="nav-items">
+              홈
+            </Link>
+            <div className="nav-item-line" />
+            <Link to="/portfolio" className="nav-items">
+              포트폴리오 만들기
+            </Link>
+            <div className="nav-item-line" />
+            <Link to="/myportfolio" className="nav-items">
+              내 포트폴리오
+            </Link>
+          </div>
+        </div>
+        <UserIcon />
+      </div>
     </div>
   )
 }
---
+++ front/src/components/UserIcon.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './NavBar.scss'
+import user from '../assets/user.png'
+
+function UserIcon() {
+  return <img src={user} alt="devgom user" className="user-icon" />
+}
+
+export default UserIcon
