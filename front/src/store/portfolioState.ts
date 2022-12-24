adf1c69f20dbecd140ee1e7590ad282b73e78871
Hyunjung Lee <hyunjung20222@gmail.com>
2022-12-25 08:42:33 +0900

    feature : 프로젝트 입력폼 Type/state 생성 (#37)

--- front/src/store/portfolioState.ts
+++ front/src/store/portfolioState.ts
@@ -1,9 +1,29 @@
-import { atom, selector } from 'recoil'
+import { atom } from 'recoil'
 
-import { PortfolioItems, PorfolioItemsType } from './portfolioType'
+import {
+  PortfolioItems,
+  PorfolioItemsType,
+  IProjectTypes,
+} from './portfolioType'
 
 // 포트폴리오 항목 순서 state
 export const pfItemsOrderState = atom<Array<PorfolioItemsType>>({
   key: 'pfItemsOrderState',
   default: PortfolioItems,
+})
+
+// 포트폴리오 중 프로젝트 항목 state
+export const pfProjectItemsState = atom<IProjectTypes>({
+  key: 'pfProjectItemsState',
+  default: {
+    ProjectName: '',
+    ProjectStartDate: '',
+    ProjectEndDate: '',
+    ProjectDescript: '',
+    ProjectPplNum: 1,
+    ProjectWork: '',
+    ProjectLink: [],
+    ProjectAchieve: '',
+    ProjectRemind: '',
+  },
 })
