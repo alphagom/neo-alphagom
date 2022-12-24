5356eaa2b60794f05623a8c1836cf523aa9520d3
Hyunjung Lee <hyunjung20222@gmail.com>
2022-12-25 08:41:06 +0900

    feature : 입력폼 textarea 컴포넌트 구현 (#45)

--- front/src/pages/portfolio/components/itemforms/itemformsitem/ItemContentTextareaLong.tsx
+++ front/src/pages/portfolio/components/itemforms/itemformsitem/ItemContentTextareaLong.tsx
@@ -1,1 +1,25 @@
-export default {}
+import React from 'react'
+
+function ItemContentTextareaLong({
+  placeholder,
+  content,
+  setFunction,
+}: {
+  placeholder: string
+  content: string
+  setFunction(value: string): void
+}) {
+  return (
+    <div>
+      <textarea
+        placeholder={placeholder}
+        value={content}
+        onChange={(e) => {
+          setFunction(e.target.value)
+        }}
+      />
+    </div>
+  )
+}
+
+export default ItemContentTextareaLong
