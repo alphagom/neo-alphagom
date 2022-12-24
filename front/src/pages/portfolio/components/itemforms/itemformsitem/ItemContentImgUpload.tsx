84cbd3539deb6d3197411a51940dbd0c7b02d691
Hyunjung Lee <hyunjung20222@gmail.com>
2022-12-25 08:41:23 +0900

    feature : 입력폼 이미지 업로드 컴포넌트 구현 (#45)

--- front/src/pages/portfolio/components/itemforms/itemformsitem/ItemContentImgUpload.tsx
+++ front/src/pages/portfolio/components/itemforms/itemformsitem/ItemContentImgUpload.tsx
@@ -1,1 +1,39 @@
-export default {}
+import React, { useCallback } from 'react'
+
+function ItemContentImgUpload({
+  imageRef,
+  setImageState,
+}: {
+  imageRef: React.MutableRefObject<HTMLInputElement | null>
+  setImageState(value: string): void
+}) {
+  // 이미지 업로드와 미리보기
+  const doUploadImage = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      if (!e.target.files) {
+        return
+      }
+      const image = e.target.files[0]
+      const CurrentImgUrl = URL.createObjectURL(image)
+
+      const formData = new FormData()
+      formData.append('image', image)
+
+      setImageState(CurrentImgUrl)
+    },
+    []
+  )
+
+  return (
+    <div>
+      <input
+        type="file"
+        ref={imageRef}
+        accept="image/jpg, image/png, image/jpeg, image/gif"
+        onChange={doUploadImage}
+      />
+    </div>
+  )
+}
+
+export default ItemContentImgUpload
