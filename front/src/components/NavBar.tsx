e5347629865ab6048965266c97101522b03bdb4c
foursoob <foursoob@naver.com>
2023-02-07 01:02:20 +0900

    feature: UserIcon 구현 (#73) 20230206 1. 드롭다운 라이브러리 설치 2. 클릭 시 뜨는 포지션 고정하려다 실패 3. 현재는 클릭 한 위치 기준으로 드롭다운이 뜹니다.

--- front/src/components/NavBar.tsx
+++ front/src/components/NavBar.tsx
@@ -1,13 +1,63 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useRef, useEffect, useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { Menu, Item, Separator, useContextMenu } from 'react-contexify'
 import './NavBar.scss'
+import 'react-contexify/dist/ReactContexify.css'
 import logo from '../assets/devgom.png'
 import title from '../assets/title.png'
 import UserIcon from './UserIcon'
 
+const MENU_ID = 'menu-id'
+
 function NavBar() {
+  // 드롭다운 포지션 고정하려그랬는데 굳이 필요할까?
+  // const [btnNodeLeft, setBtnNodeLeft] = useState<number>(0)
+  // const [btnNodeTop, setBtnNodeTop] = useState<number>(0)
+  // const triggerBtnRef = useRef<HTMLButtonElement>(null)
+  // useEffect(() => {
+  //   if (triggerBtnRef.current) {
+  //     setBtnNodeLeft(triggerBtnRef.current.getBoundingClientRect().left)
+  //     setBtnNodeTop(triggerBtnRef.current.getBoundingClientRect().top)
+  //     console.log(btnNodeLeft)
+  //     console.log(btnNodeTop)
+  //   }
+  // }, [btnNodeLeft, btnNodeTop, triggerBtnRef])
+
+  const navigate = useNavigate()
+
+  const { show } = useContextMenu({
+    id: MENU_ID,
+  })
+
+  function displayMenu(e: React.MouseEvent) {
+    show({
+      event: e,
+      // 포지션 안 정하면 클릭한 곳 아래에 메뉴 뜬다
+
+      // position: {
+      //  // 이 위치 창 크기 바뀌면 안 먹히는데 와이,,
+      //   x: btnNodeLeft + 20,
+      //   y: btnNodeTop + 70,
+      // },
+    })
+    return e
+  }
+
+  const clickProfileHandler = (e: React.MouseEvent) => {
+    displayMenu(e)
+    // console.log(btnNodeLeft)
+    // console.log(btnNodeTop)
+  }
   return (
     <div className="navbar">
+      {/* Context Menu */}
+      <Menu id={MENU_ID}>
+        <Item onClick={() => navigate(`/myportfolio`)}>내 포트폴리오</Item>
+        <Separator />
+        {/* 로그인 모달 띄우기 */}
+        <Item onClick={() => navigate(`/myportfolio`)}>내 포트폴리오</Item>
+      </Menu>
+
       <div className="navbar-content">
         <a href="/">
           <img src={logo} alt="devgom logo" width="155" />
@@ -31,7 +81,14 @@ function NavBar() {
           </div>
         </div>
       </div>
-      <UserIcon />
+      <button
+        className="nav-dropdown-btn"
+        type="button"
+        // ref={triggerBtnRef}
+        onClick={clickProfileHandler}
+      >
+        <UserIcon />
+      </button>
     </div>
   )
 }
